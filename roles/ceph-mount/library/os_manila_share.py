021eaaaf9987687b38fe7a8c6a32c79bee17dc1c
John Garbutt <john@johngarbutt.com>
2017-11-16 17:02:07 +0000

    Correctly wire up ceph_mount variables

--- roles/ceph-mount/library/os_manila_share.py
+++ roles/ceph-mount/library/os_manila_share.py
@@ -8,38 +8,34 @@ import os_client_config
 ANSIBLE_METADATA = {'metadata_version': '1.0'}
 
 
-def main():
-    module = AnsibleModule(
-        argument_spec = dict(
-            name=dict(required=True, type='str'),
-            user=dict(required=True, type='str'),
-            type=dict(required=False, type='str'),
-            protocol=dict(required=False, type='str'),
-            size_gb=dict(required=False, type='int'),
-        ),
-        supports_check_mode=False
-    )
-
+def get_share_client(module):
+    # NOTE: set OS_CLOUD environment variable to choose from clouds.yaml
     try:
         cloud_config = os_client_config.get_config()
         share_client = cloud_config.get_session_client("sharev2")
     except Exception, e:
-        module.fail_json(msg="Unable to initialise OpenStack client: %s" % e)
+        module.fail_json(
+            msg="Please check your OpenStack credentials: %s" % e)
 
-    if not share_client:
-        module.fail_json(msg="Please check your OpenStack credentials.")
+    return share_client
 
+
+def get_share_details(share_client, share_name):
     raw_shares = share_client.get(
         "/shares/detail?name=%s" % "jmfg2-spark-test").json()['shares']
     if not raw_shares:
-        raise Exception("Invalid share name")
+        # TODO - we could create the share, if given enough info
+        raise Exception("Unable to find requested share.")
 
     raw_share = raw_shares[0]
 
-    # TODO if size doesn't match, should expand the share
     share = {
+        "name": raw_share['name'],
         "id": raw_share['id'],
         "size" : raw_share['size'],
+        "protocol": raw_share['share_proto'],
+        "type": raw_share['share_type_name'],
+        "type_id": raw_share['share_type'],
     }
 
     # TODO - some drivers have a preferred export.
@@ -57,19 +53,59 @@ def main():
     if split_len == 3:
         share['host'] = "%s:%s" % (split_export[0], split_export[1])
 
+    return share
+
+
+def get_access_key(share_client, share, user):
     headers={"X-Openstack-Manila-Api-Version": "2.40"}
     payload = {"access_list": None}
     raw_access = share_client.post(
             "/shares/%s/action" % share['id'],
             json=payload, headers=headers).json()['access_list']
+
     access_key = None
     for access in raw_access:
         if access['access_to'] == "spark":
             access_key = access['access_key']
             break
 
-    # TODO - get access if this user doesn't have access
+    return access_key
+
+
+def main():
+    module = AnsibleModule(
+        argument_spec = dict(
+            name=dict(required=True, type='str'),
+            user=dict(required=True, type='str'),
+            protocol=dict(required=False, type='str'),
+            size_gb=dict(required=False, type='int'), # only checks the size
+            type=dict(required=False, type='str'),  # unused - for creation
+        ),
+        supports_check_mode=False
+    )
+
+    share_client = get_share_client(module)
+
+    share_name = module.params['name']
+    share = get_share_details(share_client, share_name)
+
+    user = module.params['user']
+    share_id = share['id']
+    access_key = get_access_key(share_client, share_id, user)
+
+    if not access_key:
+        # TODO we could add access here
+        module.fail_json(msg="User does not have access to share.")
     share['access_key'] = access_key
+
+    required_protocal = module.params['protocol']
+    if share['protocol'] != required_protocal:
+        module.fail_json(msg="Share does not match requested protocol.")
+
+    required_size = module.params['size']
+    if share['size'] != required_size:
+        # TODO if size doesn't match, we could expand the share
+        module.fail_json(msg="Share does not match requested size.")
 
     module.exit_json(changed=False, details=share)
 
