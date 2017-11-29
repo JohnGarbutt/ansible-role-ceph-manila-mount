c5c0804abecd8ecaa8aa358045f4b896b46f9bdd
John Garbutt <john@johngarbutt.com>
2017-11-29 10:01:19 +0000

    Increase memory given to spark

--- roles/hibd-spark/templates/spark-env.sh
+++ roles/hibd-spark/templates/spark-env.sh
@@ -2,9 +2,11 @@ export SPARK_CONF_DIR=/opt/spark/conf
 export SPARK_MASTER_HOST={{ groups['master'][0] }}
 export SPARK_LOCAL_IP=`hostname -s`
 
-export SPARK_WORKER_MEMORY=96g
-export SPARK_WORKER_CORES=120
-export SPARK_DAEMON_MEMORY=90g
+export SPARK_WORKER_MEMORY=120g
+export SPARK_WORKER_CORES=128
+export SPARK_DAEMON_MEMORY=120g
 
 export PYSPARK_DRIVER_PYTHON={{ hibd_spark_py3_venv }}/bin/python3
 export PYSPARK_PYTHON={{ hibd_spark_py3_venv }}/bin/python3
+
+export SPARK_HOME=/opt/spark
