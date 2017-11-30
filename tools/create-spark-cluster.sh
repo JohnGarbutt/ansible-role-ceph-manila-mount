1b047a776c98fed51a40b4510155407a3cc526dc
John Garbutt <john@johngarbutt.com>
2017-11-30 12:16:13 +0000

    Add script to create spark cluster
    Eventually also create the inventory file

---
+++ tools/create-spark-cluster.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -eux
+
+pip install python-saharaclient
+
+CLUSTER_NAME=${CLUSTER_NAME:-spark-rdma}
+
+CLUSTER_TEMPLATE_NAME=${CLUSTER_TEMPLATE_NAME:-rdma-spark-2-1-0}
+IMAGE_NAME=${IMAGE_NAME:-sahara-vanilla-2.8.0-centos7}
+KEYPAIR_NAME=${KEYPAIR_NAME:-alaska-gate}
+NETWORK_NAME=${NETWORK_NAME:-ilab}
+
+# Create a cluster.
+if ! openstack dataprocessing cluster show ${CLUSTER_NAME} >/dev/null 2>&1; then
+    openstack dataprocessing cluster create \
+        --name ${CLUSTER_NAME} \
+        --cluster-template ${CLUSTER_TEMPLATE_NAME} \
+        --image ${IMAGE_NAME} \
+        --user-keypair ${KEYPAIR_NAME} \
+        --neutron-network ${NETWORK_NAME}
+fi
