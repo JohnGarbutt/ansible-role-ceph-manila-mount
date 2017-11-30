20b005896fca9bb4ad59b01f529426c2a46a6027
John Garbutt <john@johngarbutt.com>
2017-11-30 13:59:32 +0000

    Look up id

--- tools/list-sahara-instances.py
+++ tools/list-sahara-instances.py
@@ -10,6 +10,17 @@ def get_sahara_client():
     return cloud_config.get_session_client("data_processing")
 
 
+def get_id(client, cluster_name):
+    url = "/clusters/"
+    raw_clusters = client.get(url)['clusters']
+
+    matches = [cluster['id'] for cluster in raw_clusters
+                             if cluster['name'] == cluster_name]
+    if len(matches) == 1:
+        return matches[0]
+    raise Exception("Can't find %s" % cluster_name)
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('cluster_name', type=str,
@@ -18,9 +29,12 @@ def main():
 
     client = get_sahara_client()
 
-    url = "/clusters/%s" % args.cluster_name
+    cluster_id = get_id(client, args.cluster_name)
+
+    url = "/clusters/%s" % cluster_id
     import pprint
     pprint.pprint(client.get(url))
+
 
 if __name__ == '__main__':
     main()
